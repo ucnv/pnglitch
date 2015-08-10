48d0dbe0720e479d22002f3c99ffaba004422871
ucnv <ucnvvv@gmail.com>
2015-08-10 14:24:14 +0900

    Changed images, added some lines

--- files/a.js
+++ files/a.js
@@ -1,6 +1,6 @@
 $(function() {
   var lazyLoad = function(evt) {
-    $('.catalog img').each(function(i) {
+    $('.full img, .catalog img').each(function(i) {
       var img = $(this);
       if (img.attr('src') != 'files/blank.png') return;
       if (img.position().top > $(window).scrollTop() + $(window).height() * 2) return;
